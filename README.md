f92a97624320a86fe2604c062b541dbdfd67c1aa
unknown <evaristedjoumessi9@gmail.com>
2026-06-09 20:38:27 +0100

    upgrade

---
+++ README.md
@@ -0,0 +1,12 @@
+# RTCP CHAT
+    Ce projet est un systeme de chat peer to peer basee sur WebRTC et utilisant un seveur express et socket.io.
+    Ce projet permet principalement d'initier une communication peer to peer entre deux navigateur permettant ainsi
+    de passer des appels video et d,envoyer des messages textes.
+
+    il comporte les fichiers suivants:
+        backend.js:
+         ce fichier comporte le code du back-end
+        
+        index.html:
+            Ce fichier comporte l'implementation front-end de WebRTC
+    
--- backend.js
+++ backend.js
@@ -19,23 +19,39 @@ const io = new Server(server, {
 
 io.on("connection", socket=>{
     console.log(`Nouvelle connection:\n  id: ${socket.id} \n`);
-    socket.on("join-room", roomId =>{ //Ici on gere l'accession au room
-        socket.join(roomId);
-        console.log("Connection de l'utilisateur "+socket.id+" a la room")
-    });
+    socket.emit("connected",socket.id);
+    socket.join(socket.id);
+    socket.join("room1");
+    console.log("Connection de l'utilisateur "+socket.id+" a la room")
+
+    socket.to("room1").emit("peering",socket.id);
+    console.log(socket.id+"s'appaire aux autres");
+
+    socket.on("accept-peering",(firstID)=>{
+        let idRoom = firstID+"#"+socket.id
+        socket.join(firstID+"#"+socket.id);
+        console.log("Creation dela room ",idRoom," et entree de l'utilisateur ",socket.id)
+        socket.to(firstID).emit("remote-connecting",socket.id,idRoom);
+    })
+    socket.on("peering-accepted",(remoteID)=>{
+        const id = socket.id+"#"+remoteID;
+        socket.join(id);
+        console.log(id);
+        socket.emit("generate-offer",remoteID,id);
+    })
 
     socket.on("offer", data =>{ // Ici on gere la reception des offres cote serveur et leur transmissions
-        socket.to(data.roomId).emit("offer",data.offer);
+        socket.to(data.roomId).emit("offer",data);
         console.log("offre emise")
     });
 
     socket.on("answer",(data)=>{ // Ici on gere la reception des reponses cote serveur et leur transmissions
-        socket.to(data.roomId).emit("answer",data.answer);
+        socket.to(data.roomId).emit("answer",data);
         console.log("reponse envoyee")
     })
 
     socket.on("ice-candidate", data =>{ // Ici on gere la transmissions des ice-candidates
-        socket.to(data.roomId).emit("ice-candidate", data.candidate);
+        socket.to(data.roomId).emit("ice-candidate", data);
         console.log("ice candidate envoyee")
     })
 
